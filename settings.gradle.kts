299a75d965374dcb1003c8318439faf2263f6dac
hb <55056558+concertypin@users.noreply.github.com>
2025-04-23 13:12:34 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+	id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+dependencyResolutionManagement{
+	versionCatalogs {
+		create("libs") {
+			from(files("libs.versions.toml"))
+		}
+	}
+}
+rootProject.name = "template"
+
